cfb7a1d3e61168f6400d144ca1c2c133bfa9b60a
Lartrax <lartrax909@gmail.com>
2024-03-18 23:14:20 +0100

    start coding database api

--- backend/api/database/src/main.rs
+++ backend/api/database/src/main.rs
@@ -1,6 +1,7 @@
 use ntex::{http, web};
 use ntex_cors::Cors;
 use serde::Serialize;
+use sqlx::postgres::PgPoolOptions;
 
 #[derive(Serialize)]
 struct HelloWorldResponse {
@@ -13,6 +14,36 @@ async fn hello_world(path: web::types::Path<String>) -> impl web::Responder {
 
     let json_response = HelloWorldResponse {
         greeting: format!("Hello {}!", recipient),
+    };
+
+    web::HttpResponse::Ok()
+        .content_type("application/json")
+        .json(&json_response)
+}
+
+#[derive(Serialize)]
+struct GetUserByIdResponse {
+    username: String,
+    email: String,
+}
+
+#[web::get("/user/{user_id}")]
+async fn get_user_by_id(path: web::types::Path<String>) -> impl web::Responder {
+    let user_id = path.into_inner();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://LRZ8GPmEqe-@35.228.17.43/postgres")
+        .await;
+
+    let rows = sqlx::query_as("SELECT * FROM users WHERE id = $1")
+        .bind(user_id)
+        .fetch_one(&pool)
+        .await;
+
+    let json_response = GetUserByIdResponse {
+        username: "Lars Erik".to_string(),
+        email: "lartrax909@gmail.com".to_string(),
     };
 
     web::HttpResponse::Ok()
