6a3ac9cf09093356ca953ac4efbbbcafd273dbdf
Lartrax <lartrax909@gmail.com>
2024-03-21 11:59:17 +0100

    create export page

--- frontend/timeforing-app/src/pages/Export/Export.tsx
+++ frontend/timeforing-app/src/pages/Export/Export.tsx
@@ -1,6 +1,7 @@
-import { type Component } from "solid-js";
+import { For, type Component } from "solid-js";
 
 import styles from "./Export.module.css";
+import { appState } from "../../state/store";
 
 const Export: Component = () => {
   return (
@@ -9,10 +10,93 @@ const Export: Component = () => {
         display: "flex",
         "flex-direction": "column",
         gap: "2em",
-        width: "30vmax",
+        width: "40vmax",
       }}
     >
-      <p>Eksporter til agresso</p>
+      <p style={{ "font-size": "2em" }}>
+        Week {appState.activeWorkWeek.week}, {appState.activeWorkWeek.year}
+      </p>
+      <div
+        style={{
+          display: "flex",
+          "flex-direction": "column",
+          gap: "1em",
+          border: "1px solid #eee",
+          "border-radius": "1em",
+          padding: "1em",
+        }}
+      >
+        <For each={appState.activeWorkWeek.days}>
+          {(day) => (
+            <span
+              style={{
+                display: "flex",
+                "flex-direction": "column",
+                "text-align": "left",
+                gap: "0.5em",
+              }}
+            >
+              <p>{day.day}</p>
+
+              <For each={day.codes}>
+                {(code) => (
+                  <span
+                    style={{
+                      display: "flex",
+                      "justify-content": "space-around",
+                      padding: "0.5em",
+                      "border-radius": "1em",
+                      background: "#fafafa",
+                    }}
+                  >
+                    <p
+                      style={{
+                        "flex-grow": 1,
+                        "flex-basis": 0,
+                        "text-align": "left",
+                        "padding-left": "1em",
+                      }}
+                    >
+                      {code.code}
+                    </p>
+                    <div
+                      style={{
+                        "border-left": "1px solid #eee",
+                        "padding-left": "1em",
+                      }}
+                    />
+                    <p
+                      style={{
+                        "flex-grow": 2,
+                        "flex-basis": 0,
+                        "text-align": "left",
+                      }}
+                    >
+                      {code.description}
+                    </p>
+                    <div
+                      style={{
+                        "border-right": "1px solid #eee",
+                        "padding-left": "1em",
+                      }}
+                    />
+                    <p
+                      style={{
+                        "flex-grow": 1,
+                        "flex-basis": 0,
+                        "text-align": "right",
+                        "padding-right": "1em",
+                      }}
+                    >
+                      {code.hours}h
+                    </p>
+                  </span>
+                )}
+              </For>
+            </span>
+          )}
+        </For>
+      </div>
     </div>
   );
 };
